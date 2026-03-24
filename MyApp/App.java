2eb8c43a5f667a53b7e57c77123b0ebe169f0069
Aishwarya Batra <aishwaryabatra10@gmail.com>
2026-03-24 08:53:49 +0530

    Implemented room abstraction and inheritance with availability display

--- MyApp/App.java
+++ MyApp/App.java
@@ -1,6 +1,69 @@
-class MyApp{
-    public static void main(String[] args){
-        System.out.println("Hello ");
-        System.out.println("welcome to the Hotel management app");
+abstract class Room {
+    String roomType;
+    int availableRooms;
+    double price;
+
+    Room(String roomType, int availableRooms, double price) {
+        this.roomType = roomType;
+        this.availableRooms = availableRooms;
+        this.price = price;
+    }
+
+    abstract void displayRoomDetails();
+}
+
+class StandardRoom extends Room {
+    StandardRoom(int availableRooms, double price) {
+        super("Standard Room", availableRooms, price);
+    }
+
+    void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Available Rooms: " + availableRooms);
+        System.out.println("Price per night: " + price);
+        System.out.println("---------------------------");
+    }
+}
+
+class DeluxeRoom extends Room {
+    DeluxeRoom(int availableRooms, double price) {
+        super("Deluxe Room", availableRooms, price);
+    }
+
+    void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Available Rooms: " + availableRooms);
+        System.out.println("Price per night: " + price);
+        System.out.println("---------------------------");
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom(int availableRooms, double price) {
+        super("Suite Room", availableRooms, price);
+    }
+
+    void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Available Rooms: " + availableRooms);
+        System.out.println("Price per night: " + price);
+        System.out.println("---------------------------");
+    }
+}
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay App");
+        System.out.println("Room Availability:\n");
+
+        Room standard = new StandardRoom(5, 2000);
+        Room deluxe = new DeluxeRoom(3, 3500);
+        Room suite = new SuiteRoom(2, 5000);
+
+        standard.displayRoomDetails();
+        deluxe.displayRoomDetails();
+        suite.displayRoomDetails();
+
+        System.out.println("Application Terminated");
     }
 }
