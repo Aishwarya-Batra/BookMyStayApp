c50fde62b0abef8c960b50165439be8a5a0fabea
Aishwarya Batra <aishwaryabatra10@gmail.com>
2026-03-24 09:05:08 +0530

    UC5 Implemented booking request queue using FCFS with Reservation and BookingRequestQueue classes

--- MyApp/App.java
+++ MyApp/App.java
@@ -1,105 +1,60 @@
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
-abstract class Room {
-    String roomType;
-    double price;
+class Reservation {
+    private String guestName;
+    private String roomType;
 
-    Room(String roomType, double price) {
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
         this.roomType = roomType;
-        this.price = price;
+    }
+
+    public String getGuestName() {
+        return guestName;
     }
 
     public String getRoomType() {
         return roomType;
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    abstract void displayRoomDetails();
-}
-
-class StandardRoom extends Room {
-    StandardRoom() {
-        super("Standard Room", 2000);
-    }
-
-    void displayRoomDetails() {
-        System.out.println(roomType + " - Price: " + price);
+    public void displayReservation() {
+        System.out.println("Guest: " + guestName + ", Room Type: " + roomType);
     }
 }
 
-class DeluxeRoom extends Room {
-    DeluxeRoom() {
-        super("Deluxe Room", 3500);
+class BookingRequestQueue {
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
     }
 
-    void displayRoomDetails() {
-        System.out.println(roomType + " - Price: " + price);
-    }
-}
-
-class SuiteRoom extends Room {
-    SuiteRoom() {
-        super("Suite Room", 5000);
+    public void addRequest(Reservation reservation) {
+        requestQueue.add(reservation);
+        System.out.println("Booking request added for " + reservation.getGuestName());
     }
 
-    void displayRoomDetails() {
-        System.out.println(roomType + " - Price: " + price);
-    }
-}
-
-class RoomInventory {
-    private HashMap<String, Integer> inventory;
-
-    public RoomInventory() {
-        inventory = new HashMap<>();
-    }
-
-    public void addRoom(String type, int count) {
-        inventory.put(type, count);
-    }
-
-    public int getAvailability(String type) {
-        return inventory.getOrDefault(type, 0);
-    }
-
-    public HashMap<String, Integer> getInventory() {
-        return inventory;
-    }
-}
-
-class SearchService {
-    public void displayAvailableRooms(RoomInventory inventory, Room[] rooms) {
-        System.out.println("Available Rooms:");
-
-        for (Room room : rooms) {
-            int available = inventory.getAvailability(room.getRoomType());
-            if (available > 0) {
-                room.displayRoomDetails();
-                System.out.println("Available Count: " + available);
-                System.out.println("----------------------");
-            }
+    public void displayRequests() {
+        System.out.println("\nBooking Requests in Queue (FCFS Order):");
+        for (Reservation r : requestQueue) {
+            r.displayReservation();
         }
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        RoomInventory inventory = new RoomInventory();
+        BookingRequestQueue queue = new BookingRequestQueue();
 
-        inventory.addRoom("Standard Room", 5);
-        inventory.addRoom("Deluxe Room", 0);
-        inventory.addRoom("Suite Room", 2);
+        Reservation r1 = new Reservation("Aishwarya", "Standard Room");
+        Reservation r2 = new Reservation("Rahul", "Deluxe Room");
+        Reservation r3 = new Reservation("Sneha", "Suite Room");
 
-        Room[] rooms = {
-                new StandardRoom(),
-                new DeluxeRoom(),
-                new SuiteRoom()
-        };
+        queue.addRequest(r1);
+        queue.addRequest(r2);
+        queue.addRequest(r3);
 
-        SearchService searchService = new SearchService();
-        searchService.displayAvailableRooms(inventory, rooms);
+        queue.displayRequests();
     }
 }
